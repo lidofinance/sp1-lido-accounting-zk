3fc11a9b54d47fcc61bd170300c3fb0644a8fd29
color-typea <colorcode6a@gmail.com>
2024-08-31 19:03:04 +0800

    nit: put two possible sizes for gen_pair into code

--- shared/examples/gen_synthetic_bs_pair.rs
+++ shared/examples/gen_synthetic_bs_pair.rs
@@ -10,6 +10,58 @@ use sp1_lido_accounting_zk_shared::beacon_state_reader::{BeaconStateReader, File
 
 use simple_logger::SimpleLogger;
 
+fn small_problem_size(old_slot: u64, new_slot: u64) -> (GenerationSpec, GenerationSpec) {
+    let base_state_spec = GenerationSpec {
+        slot: old_slot,
+        non_lido_validators: 2_u64.pow(11),
+        deposited_lido_validators: 2_u64.pow(11),
+        exited_lido_validators: 2_u64.pow(7),
+        pending_deposit_lido_validators: 2_u64.pow(5),
+        balances_generation_mode: BalanceGenerationMode::FIXED,
+        shuffle: false,
+        base_slot: None,
+        overwrite: true,
+    };
+    let update_state_spec = GenerationSpec {
+        slot: new_slot,
+        non_lido_validators: 2_u64.pow(7),
+        deposited_lido_validators: 2_u64.pow(7),
+        exited_lido_validators: 2_u64.pow(3),
+        pending_deposit_lido_validators: 2_u64.pow(3),
+        balances_generation_mode: BalanceGenerationMode::FIXED,
+        shuffle: false,
+        base_slot: Some(base_state_spec.slot),
+        overwrite: true,
+    };
+    return (base_state_spec, update_state_spec);
+}
+
+fn large_problem_size(old_slot: u64, new_slot: u64) -> (GenerationSpec, GenerationSpec) {
+    let base_state_spec = GenerationSpec {
+        slot: old_slot,
+        non_lido_validators: 2_u64.pow(16),
+        deposited_lido_validators: 2_u64.pow(16),
+        exited_lido_validators: 2_u64.pow(12),
+        pending_deposit_lido_validators: 2_u64.pow(9),
+        balances_generation_mode: BalanceGenerationMode::FIXED,
+        shuffle: false,
+        base_slot: None,
+        overwrite: true,
+    };
+    let update_state_spec = GenerationSpec {
+        slot: new_slot,
+        non_lido_validators: 2_u64.pow(11),
+        deposited_lido_validators: 2_u64.pow(11),
+        exited_lido_validators: 2_u64.pow(5),
+        pending_deposit_lido_validators: 2_u64.pow(5),
+        balances_generation_mode: BalanceGenerationMode::FIXED,
+        shuffle: false,
+        base_slot: Some(base_state_spec.slot),
+        overwrite: true,
+    };
+    return (base_state_spec, update_state_spec);
+}
+
 #[tokio::main]
 async fn main() {
     SimpleLogger::new().env().init().unwrap();
@@ -18,28 +70,9 @@ async fn main() {
     let creator = SyntheticBeaconStateCreator::new(&file_store, false, true);
     let old_slot = 2000000;
     let new_slot = 2100000;
-    let base_state_spec = GenerationSpec {
-        slot: old_slot,
-        non_lido_validators: 2_u64.pow(10),
-        deposited_lido_validators: 2_u64.pow(9),
-        exited_lido_validators: 2_u64.pow(3),
-        pending_deposit_lido_validators: 2_u64.pow(2),
-        balances_generation_mode: BalanceGenerationMode::FIXED,
-        shuffle: false,
-        base_slot: None,
-        overwrite: true,
-    };
-    let update_state_spec = GenerationSpec {
-        slot: new_slot,
-        non_lido_validators: 2_u64.pow(5),
-        deposited_lido_validators: 2_u64.pow(4),
-        exited_lido_validators: 2_u64.pow(2),
-        pending_deposit_lido_validators: 2_u64.pow(1),
-        balances_generation_mode: BalanceGenerationMode::FIXED,
-        shuffle: false,
-        base_slot: Some(base_state_spec.slot),
-        overwrite: true,
-    };
+
+    let (base_state_spec, update_state_spec) = small_problem_size(old_slot, new_slot);
+    // let (base_state_spec, update_state_spec) = large_problem_size(old_slot, new_slot);
 
     creator
         .create_beacon_state(base_state_spec)
