f56cb4369f56be1141c4e078e7fc7f8536136c8e
color-typea <colorcode6a@gmail.com>
2024-09-07 19:43:41 +0800

    Fix contract tests

--- contracts/test/ValidatorsMerkleVerifier.t.sol
+++ contracts/test/ValidatorsMerkleVerifier.t.sol
@@ -7,7 +7,6 @@ import {Sp1LidoAccountingReportContractControllable} from "../src/Sp1LidoAccount
 import {LidoValidatorState, Report, ReportMetadata} from "../src/Sp1LidoAccountingReportContractBase.sol";
 import {SP1VerifierGateway} from "@sp1-contracts/SP1VerifierGateway.sol";
 
-
 contract Sp1LidoAccountingReportContractTest is Test {
     using stdJson for string;
 
@@ -26,9 +25,8 @@ contract Sp1LidoAccountingReportContractTest is Test {
         string memory root = vm.projectRoot();
         string memory path = string.concat(root, "/src/fixtures/fixture.json");
         string memory json = vm.readFile(path);
-        bytes memory jsonBytes = json.parseRaw(".");
         // This should be
-        // return abi.decode(jsonBytes, (SP1ProofFixtureJson));
+        // return abi.decode(json.parseRaw("."), (SP1ProofFixtureJson));
         // ... but it reverts with no explanation - so just doing it manually
         return (
             SP1ProofFixtureJson(
@@ -45,8 +43,12 @@ contract Sp1LidoAccountingReportContractTest is Test {
                     json.readBytes32(".metadata.lido_withdrawal_credentials"),
                     json.readBytes32(".metadata.beacon_block_hash"),
                     LidoValidatorState(
-                        json.readUint(".metadata.state_for_previous_report.slot"),
-                        json.readBytes32(".metadata.state_for_previous_report.merkle_root")
+                        json.readUint(
+                            ".metadata.state_for_previous_report.slot"
+                        ),
+                        json.readBytes32(
+                            ".metadata.state_for_previous_report.merkle_root"
+                        )
                     ),
                     LidoValidatorState(
                         json.readUint(".metadata.new_state.slot"),
@@ -63,164 +65,201 @@ contract Sp1LidoAccountingReportContractTest is Test {
         SP1ProofFixtureJson memory fixture = loadFixture();
 
         verifier = address(new SP1VerifierGateway(address(1)));
-        _contract = new Sp1LidoAccountingReportContractControllable(verifier, fixture.vkey, fixture.metadata.old_state);
-    }
-
-    function setExternalDependencies(
-        uint256 slot, 
-        bytes32 beacon_block_hash,
-        bytes32 withdrawal_credentials
-    ) private {
-        _contract.setBeaconBlockHash(slot, beacon_block_hash);
-        _contract.setWithdrawalCredentials(withdrawal_credentials);
+        _contract = new Sp1LidoAccountingReportContractControllable(
+            verifier,
+            fixture.vkey,
+            fixture.metadata.lido_withdrawal_credentials,
+            fixture.metadata.old_state
+        );
     }
 
     function test_validProof() public {
         SP1ProofFixtureJson memory fixture = loadFixture();
-        setExternalDependencies(
-            fixture.metadata.slot, 
-            fixture.metadata.beacon_block_hash, 
-            fixture.metadata.lido_withdrawal_credentials 
+        _contract.setBeaconBlockHash(
+            fixture.metadata.slot,
+            fixture.metadata.beacon_block_hash
         );
         vm.mockCall(
-            verifier, 
-            abi.encodeWithSelector(SP1VerifierGateway.verifyProof.selector), 
+            verifier,
+            abi.encodeWithSelector(SP1VerifierGateway.verifyProof.selector),
             abi.encode()
         );
 
-        _contract.verify(fixture.metadata.slot, fixture.report, fixture.metadata, fixture.proof, fixture.publicValues);
+        _contract.verify(
+            fixture.metadata.slot,
+            fixture.report,
+            fixture.metadata,
+            fixture.proof,
+            fixture.publicValues
+        );
     }
 
     function test_validProofWrongExpectedSlot_reverts() public {
         SP1ProofFixtureJson memory fixture = loadFixture();
-        setExternalDependencies(
-            fixture.metadata.slot, 
-            fixture.metadata.beacon_block_hash, 
-            fixture.metadata.lido_withdrawal_credentials
+
+        _contract.setBeaconBlockHash(
+            fixture.metadata.slot,
+            fixture.metadata.beacon_block_hash
         );
         vm.mockCall(
-            verifier, 
-            abi.encodeWithSelector(SP1VerifierGateway.verifyProof.selector), 
+            verifier,
+            abi.encodeWithSelector(SP1VerifierGateway.verifyProof.selector),
             abi.encode()
         );
 
         vm.expectRevert("Slot mismatch: report");
-        _contract.verify(111111, fixture.report, fixture.metadata, fixture.proof, fixture.publicValues);
+        _contract.verify(
+            111111,
+            fixture.report,
+            fixture.metadata,
+            fixture.proof,
+            fixture.publicValues
+        );
     }
 
     function test_validProofWrongExpectedBeaconBlockHash_reverts() public {
         SP1ProofFixtureJson memory fixture = loadFixture();
         bytes32 expectedHash = 0x1111111100000000000000000000000000000000000000000000000022222222;
 
-        setExternalDependencies(
-            fixture.metadata.slot, 
-            expectedHash, 
-            fixture.metadata.lido_withdrawal_credentials
-        );
+        _contract.setBeaconBlockHash(fixture.metadata.slot, expectedHash);
         vm.mockCall(
-            verifier, 
-            abi.encodeWithSelector(SP1VerifierGateway.verifyProof.selector), 
+            verifier,
+            abi.encodeWithSelector(SP1VerifierGateway.verifyProof.selector),
             abi.encode()
         );
         vm.expectRevert("BeaconBlockHash mismatch");
 
-        _contract.verify(fixture.metadata.slot, fixture.report, fixture.metadata, fixture.proof, fixture.publicValues);
+        _contract.verify(
+            fixture.metadata.slot,
+            fixture.report,
+            fixture.metadata,
+            fixture.proof,
+            fixture.publicValues
+        );
     }
 
     function test_validProofWrongLidoWithdrawalCredentials_reverts() public {
         SP1ProofFixtureJson memory fixture = loadFixture();
-        bytes32 expectedCredentials = 0xABCDEF0000000000000000000000000000000000000000000000000000FEDCBA;
 
-        setExternalDependencies(
-            fixture.metadata.slot, 
-            fixture.metadata.beacon_block_hash, 
-            expectedCredentials
+        _contract.setBeaconBlockHash(
+            fixture.metadata.slot,
+            fixture.metadata.beacon_block_hash
         );
         vm.mockCall(
-            verifier, 
-            abi.encodeWithSelector(SP1VerifierGateway.verifyProof.selector), 
+            verifier,
+            abi.encodeWithSelector(SP1VerifierGateway.verifyProof.selector),
             abi.encode()
         );
 
+        fixture.metadata.lido_withdrawal_credentials = 0xABCDEF0000000000000000000000000000000000000000000000000000FEDCBA;
         vm.expectRevert("Withdrawal credentials mismatch");
-        _contract.verify(fixture.metadata.slot, fixture.report, fixture.metadata, fixture.proof, fixture.publicValues);
+        _contract.verify(
+            fixture.metadata.slot,
+            fixture.report,
+            fixture.metadata,
+            fixture.proof,
+            fixture.publicValues
+        );
     }
 
     function test_invalidProof_reverts() public {
         SP1ProofFixtureJson memory fixture = loadFixture();
-        setExternalDependencies(
-            fixture.metadata.slot, 
-            fixture.metadata.beacon_block_hash, 
-            fixture.metadata.lido_withdrawal_credentials
+        _contract.setBeaconBlockHash(
+            fixture.metadata.slot,
+            fixture.metadata.beacon_block_hash
         );
 
         // Create a fake proof.
         bytes memory fakeProof = new bytes(fixture.proof.length);
         vm.mockCallRevert(
-            verifier, 
-            abi.encodeWithSelector(SP1VerifierGateway.verifyProof.selector), 
+            verifier,
+            abi.encodeWithSelector(SP1VerifierGateway.verifyProof.selector),
             "MOCKED_REVERT"
         );
-        
+
         vm.expectRevert();
-        _contract.verify(fixture.metadata.slot, fixture.report, fixture.metadata, fakeProof, fixture.publicValues);
+        _contract.verify(
+            fixture.metadata.slot,
+            fixture.report,
+            fixture.metadata,
+            fakeProof,
+            fixture.publicValues
+        );
     }
 
     function test_invalidPublicValues_reverts() public {
         SP1ProofFixtureJson memory fixture = loadFixture();
-        setExternalDependencies(
-            fixture.metadata.slot, 
-            fixture.metadata.beacon_block_hash, 
-            fixture.metadata.lido_withdrawal_credentials
+        _contract.setBeaconBlockHash(
+            fixture.metadata.slot,
+            fixture.metadata.beacon_block_hash
         );
 
         // Create a fake public values.
         bytes memory fakePublicValues = new bytes(fixture.proof.length);
         vm.mockCallRevert(
-            verifier, 
-            abi.encodeWithSelector(SP1VerifierGateway.verifyProof.selector), 
+            verifier,
+            abi.encodeWithSelector(SP1VerifierGateway.verifyProof.selector),
             "MOCKED_REVERT"
         );
 
         vm.expectRevert();
-        _contract.verify(fixture.metadata.slot, fixture.report, fixture.metadata, fixture.proof, fakePublicValues);
+        _contract.verify(
+            fixture.metadata.slot,
+            fixture.report,
+            fixture.metadata,
+            fixture.proof,
+            fakePublicValues
+        );
     }
 
     function test_noStateRecordedForOldStateSlot_reverts() public {
         SP1ProofFixtureJson memory fixture = loadFixture();
         fixture.metadata.old_state.slot = 1111111;
 
-        setExternalDependencies(
-            fixture.metadata.slot, 
-            fixture.metadata.beacon_block_hash, 
-            fixture.metadata.lido_withdrawal_credentials
+        _contract.setBeaconBlockHash(
+            fixture.metadata.slot,
+            fixture.metadata.beacon_block_hash
         );
         vm.mockCall(
-            verifier, 
-            abi.encodeWithSelector(SP1VerifierGateway.verifyProof.selector), 
+            verifier,
+            abi.encodeWithSelector(SP1VerifierGateway.verifyProof.selector),
             abi.encode()
         );
 
         vm.expectRevert("Old state merkle_root not found");
-        _contract.verify(fixture.metadata.slot, fixture.report, fixture.metadata, fixture.proof, fixture.publicValues);
+        _contract.verify(
+            fixture.metadata.slot,
+            fixture.report,
+            fixture.metadata,
+            fixture.proof,
+            fixture.publicValues
+        );
     }
 
     function test_oldStateWrongMerkleRoot_reverts() public {
         SP1ProofFixtureJson memory fixture = loadFixture();
-        fixture.metadata.old_state.merkle_root = 0x0102030405060708090000000000000000000000000000000000000000000000;
+        fixture
+            .metadata
+            .old_state
+            .merkle_root = 0x0102030405060708090000000000000000000000000000000000000000000000;
 
-        setExternalDependencies(
-            fixture.metadata.slot, 
-            fixture.metadata.beacon_block_hash, 
-            fixture.metadata.lido_withdrawal_credentials
+        _contract.setBeaconBlockHash(
+            fixture.metadata.slot,
+            fixture.metadata.beacon_block_hash
         );
         vm.mockCall(
-            verifier, 
-            abi.encodeWithSelector(SP1VerifierGateway.verifyProof.selector), 
+            verifier,
+            abi.encodeWithSelector(SP1VerifierGateway.verifyProof.selector),
             abi.encode()
         );
 
         vm.expectRevert("Old state merkle_root mismatch");
-        _contract.verify(fixture.metadata.slot, fixture.report, fixture.metadata, fixture.proof, fixture.publicValues);
+        _contract.verify(
+            fixture.metadata.slot,
+            fixture.report,
+            fixture.metadata,
+            fixture.proof,
+            fixture.publicValues
+        );
     }
 }
