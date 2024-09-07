a9c99be7d8c6af8176cf2a9c9f03f133a1a96f00
color-typea <colorcode6a@gmail.com>
2024-09-07 19:37:22 +0800

    Refactor contaracts to accept lido withdrawal credentials as constructor argument

--- contracts/src/Sp1LidoAccountingReportContractBase.sol
+++ contracts/src/Sp1LidoAccountingReportContractBase.sol
@@ -34,15 +34,26 @@ abstract contract Sp1LidoAccountingReportContractBase {
     event ReportRejected(string reason);
     event LidoValidatorStateHashRecorded(uint256 slot, bytes32 merkle_root);
 
-    mapping (uint256 => Report) private _reports;
-    mapping (uint256 => bytes32) private _states;
-    address public verifier;
-    bytes32 public vkey;
+    mapping(uint256 => Report) private _reports;
+    mapping(uint256 => bytes32) private _states;
+    address public immutable verifier;
+    bytes32 public immutable vkey;
 
-    constructor(address _verifier, bytes32 _vkey, LidoValidatorState memory _initial_state) {
+    bytes32 public immutable widthrawal_credentials;
+
+    constructor(
+        address _verifier,
+        bytes32 _vkey,
+        bytes32 _widthrawal_credentials,
+        LidoValidatorState memory _initial_state
+    ) {
         verifier = _verifier;
         vkey = _vkey;
-        _recordLidoValidatorStateHash(_initial_state.slot, _initial_state.merkle_root);
+        widthrawal_credentials = _widthrawal_credentials;
+        _recordLidoValidatorStateHash(
+            _initial_state.slot,
+            _initial_state.merkle_root
+        );
     }
 
     function submitReportData(
@@ -56,7 +67,10 @@ abstract contract Sp1LidoAccountingReportContractBase {
 
         // If all checks pass - update report and record the state
         _updateReport(slot, report);
-        _recordLidoValidatorStateHash(metadata.new_state.slot, metadata.new_state.merkle_root);
+        _recordLidoValidatorStateHash(
+            metadata.new_state.slot,
+            metadata.new_state.merkle_root
+        );
     }
 
     function _verify(
@@ -68,38 +82,49 @@ abstract contract Sp1LidoAccountingReportContractBase {
     ) public view {
         // Check the report was not previously set
         Report storage report_at_slot = _reports[slot];
-        _require(report_at_slot.slot == 0, "Report was already accepted for a given slot");
+        _require(
+            report_at_slot.slot == 0,
+            "Report was already accepted for a given slot"
+        );
 
         // Check the report is for the target slot
         _require(report.slot == slot, "Slot mismatch: report");
         _require(metadata.new_state.slot == slot, "Slot mismatch: new state");
 
-        // Check that passed beacon_block_hash matches the one observed on the blockchain for 
+        // Check that passed beacon_block_hash matches the one observed on the blockchain for
         // the target slot
         _require(
-            metadata.beacon_block_hash == _getBeaconBlockHash(slot), 
+            metadata.beacon_block_hash == _getBeaconBlockHash(slot),
             "BeaconBlockHash mismatch"
         );
 
         // Check that correct withdrawal credentials were used
         _require(
-            metadata.lido_withdrawal_credentials == _getExpectedWithdrawalCredentials(),
+            metadata.lido_withdrawal_credentials ==
+                _getExpectedWithdrawalCredentials(),
             "Withdrawal credentials mismatch"
         );
 
         // Check that the old report hash matches the one recorded in contract
-        bytes32 old_state_hash = getLidoValidatorStateHash(metadata.old_state.slot);
+        bytes32 old_state_hash = getLidoValidatorStateHash(
+            metadata.old_state.slot
+        );
         _require(old_state_hash != 0, "Old state merkle_root not found");
-        _require(metadata.old_state.merkle_root == old_state_hash, "Old state merkle_root mismatch");
+        _require(
+            metadata.old_state.merkle_root == old_state_hash,
+            "Old state merkle_root mismatch"
+        );
 
         // Check that report and metadata match public values committed in the ZK-program
-        PublicValues memory public_values = abi.decode(publicValues, (PublicValues));
+        PublicValues memory public_values = abi.decode(
+            publicValues,
+            (PublicValues)
+        );
         _verify_public_values(report, metadata, public_values);
 
         // Verify ZK-program and public values
         ISP1Verifier(verifier).verifyProof(vkey, publicValues, proof);
     }
-
 
     function _verify_public_values(
         Report memory report,
@@ -107,36 +132,40 @@ abstract contract Sp1LidoAccountingReportContractBase {
         PublicValues memory publicValues
     ) internal pure {
         _require(
-            report.slot == publicValues.report.slot, 
+            report.slot == publicValues.report.slot,
             "Report.slot doesn't match public values"
         );
         _require(
-            report.all_lido_validators == publicValues.report.all_lido_validators, 
+            report.all_lido_validators ==
+                publicValues.report.all_lido_validators,
             "Report.all_lido_validators doesn't match public values"
         );
         _require(
-            report.exited_lido_validators == publicValues.report.exited_lido_validators, 
+            report.exited_lido_validators ==
+                publicValues.report.exited_lido_validators,
             "Report.exited_lido_validators doesn't match public values"
         );
         _require(
-            report.lido_cl_valance == publicValues.report.lido_cl_valance, 
+            report.lido_cl_valance == publicValues.report.lido_cl_valance,
             "Report.lido_cl_valance doesn't match public values"
         );
 
         _require(
-            metadata.slot == publicValues.metadata.slot, 
+            metadata.slot == publicValues.metadata.slot,
             "Metadata.slot doesn't match public values"
         );
         _require(
-            metadata.epoch == publicValues.metadata.epoch, 
+            metadata.epoch == publicValues.metadata.epoch,
             "Metadata.epoch doesn't match public values"
         );
         _require(
-            metadata.lido_withdrawal_credentials == publicValues.metadata.lido_withdrawal_credentials, 
+            metadata.lido_withdrawal_credentials ==
+                publicValues.metadata.lido_withdrawal_credentials,
             "Metadata.lido_withdrawal_credentials doesn't match public values"
         );
         _require(
-            metadata.beacon_block_hash == publicValues.metadata.beacon_block_hash, 
+            metadata.beacon_block_hash ==
+                publicValues.metadata.beacon_block_hash,
             "Metadata.beacon_block_hash doesn't match public values"
         );
 
@@ -145,7 +174,8 @@ abstract contract Sp1LidoAccountingReportContractBase {
             "Metadata.old_state.slot doesn't match public values"
         );
         _require(
-            metadata.old_state.merkle_root == publicValues.metadata.old_state.merkle_root,
+            metadata.old_state.merkle_root ==
+                publicValues.metadata.old_state.merkle_root,
             "Metadata.old_state.merkle_root doesn't match public values"
         );
 
@@ -154,12 +184,15 @@ abstract contract Sp1LidoAccountingReportContractBase {
             "Metadata.new_state.slot doesn't match public values"
         );
         _require(
-            metadata.new_state.merkle_root == publicValues.metadata.new_state.merkle_root,
+            metadata.new_state.merkle_root ==
+                publicValues.metadata.new_state.merkle_root,
             "Metadata.new_state.merkle_root doesn't match public values"
         );
     }
 
-    function getReport(uint256 slot) public view returns (Report memory result) {
+    function getReport(
+        uint256 slot
+    ) public view returns (Report memory result) {
         return (_reports[slot]);
     }
 
@@ -168,11 +201,16 @@ abstract contract Sp1LidoAccountingReportContractBase {
         emit ReportAccepted(report);
     }
 
-    function getLidoValidatorStateHash(uint256 slot) public view returns (bytes32 result) {
+    function getLidoValidatorStateHash(
+        uint256 slot
+    ) public view returns (bytes32 result) {
         return (_states[slot]);
     }
 
-    function _recordLidoValidatorStateHash(uint256 slot, bytes32 state_merkle_root) internal {
+    function _recordLidoValidatorStateHash(
+        uint256 slot,
+        bytes32 state_merkle_root
+    ) internal {
         _states[slot] = state_merkle_root;
         emit LidoValidatorStateHashRecorded(slot, state_merkle_root);
     }
@@ -183,6 +221,16 @@ abstract contract Sp1LidoAccountingReportContractBase {
         }
     }
 
-    function _getBeaconBlockHash(uint256 slot) internal virtual view returns (bytes32);
-    function _getExpectedWithdrawalCredentials() internal virtual view returns (bytes32);
+    function _getExpectedWithdrawalCredentials()
+        internal
+        view
+        virtual
+        returns (bytes32)
+    {
+        return (widthrawal_credentials);
+    }
+
+    function _getBeaconBlockHash(
+        uint256 slot
+    ) internal view virtual returns (bytes32);
 }
--- contracts/src/Sp1LidoAccountingReportContractControllable.sol
+++ contracts/src/Sp1LidoAccountingReportContractControllable.sol
@@ -3,33 +3,35 @@ pragma solidity ^0.8.20;
 
 import "./Sp1LidoAccountingReportContractBase.sol";
 
-/// @title Fibonacci.
-/// @author Succinct Labs
-/// @notice This contract implements a simple example of verifying the proof of a computing a
-///         fibonacci number.
-contract Sp1LidoAccountingReportContractControllable is Sp1LidoAccountingReportContractBase {
-    mapping (uint256 => bytes32) private _beaconBlockHashes;
-    bytes32 _withdrawalCredentials;
+contract Sp1LidoAccountingReportContractControllable is
+    Sp1LidoAccountingReportContractBase
+{
+    mapping(uint256 => bytes32) private _beaconBlockHashes;
 
-    constructor(address _verifier, bytes32 _vkey, LidoValidatorState memory _initial_state) Sp1LidoAccountingReportContractBase(_verifier, _vkey, _initial_state) {}
+    constructor(
+        address _verifier,
+        bytes32 _vkey,
+        bytes32 _widthrawal_credentials,
+        LidoValidatorState memory _initial_state
+    )
+        Sp1LidoAccountingReportContractBase(
+            _verifier,
+            _vkey,
+            _widthrawal_credentials,
+            _initial_state
+        )
+    {}
 
     function setBeaconBlockHash(uint256 slot, bytes32 beaconBlockHash) public {
         _beaconBlockHashes[slot] = beaconBlockHash;
     }
 
-
-    function _getBeaconBlockHash(uint256 slot) internal override view returns (bytes32) {
+    function _getBeaconBlockHash(
+        uint256 slot
+    ) internal view override returns (bytes32) {
         bytes32 result = _beaconBlockHashes[slot];
         require(result != 0, "Block hash is not set for target slot");
         return result;
-    }
-
-    function setWithdrawalCredentials(bytes32 withdrawalCredentials) public {
-        _withdrawalCredentials = withdrawalCredentials;
-    }
-
-    function _getExpectedWithdrawalCredentials() internal override view returns (bytes32) {
-        return _withdrawalCredentials;
     }
 
     function verify(
