46bbfd03481ca9f00832ee3f8d8140b013a814b0
color-typea <colorcode6a@gmail.com>
2024-09-08 13:59:02 +0800

    Include contract ABI into the program + build helper to ensure ABI is up to date

--- script/build.rs
+++ script/build.rs
@@ -1,5 +1,29 @@
+use std::process::Command;
+
 use sp1_helper::build_program;
 
+fn build_contract_abi(path: &str) {
+    let constracts_dir = std::path::Path::new(path);
+    print!("Building contracts in {:#?}", constracts_dir.as_os_str());
+
+    let mut command = Command::new("forge");
+    command.arg("build").current_dir(constracts_dir);
+    command.status().expect("Failed to forge build");
+
+    let dirs = vec![
+        constracts_dir.join("src"),
+        constracts_dir.join("lib"),
+        constracts_dir.join("out"), // this is a bit strange, but this line actually make it work
+        constracts_dir.join("foundry.toml"),
+    ];
+    for dir in dirs {
+        if dir.exists() {
+            println!("cargo::rerun-if-changed={}", dir.canonicalize().unwrap().display());
+        }
+    }
+}
+
 fn main() {
-    build_program("../program")
+    build_contract_abi("../contracts");
+    build_program("../program");
 }
--- script/src/lib/lib.rs
+++ script/src/lib/lib.rs
@@ -1,3 +1,6 @@
 pub mod beacon_state_reader_enum;
 
 pub const ELF: &[u8] = include_bytes!("../../../program/elf/riscv32im-succinct-zkvm-elf");
+
+pub const CONTRACT_ABI: &[u8] =
+    include_bytes!("../../../contracts/out/Sp1LidoAccountingReportContract.sol/Sp1LidoAccountingReportContract.json");
