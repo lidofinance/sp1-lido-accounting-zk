fc3b12dfefdc852ca6cb6136b43fba2cffca9a15
VP <vp@lido.fi>
2025-08-06 10:54:13 +0200

    feat: remove trailing quote

--- docker/healthcheck.sh
+++ docker/healthcheck.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-PORT=$(echo "${SERVICE_BIND_TO_ADDR:-0.0.0.0:8080}" | awk -F: '{print $NF}')
+PORT=$(echo "${SERVICE_BIND_TO_ADDR:-0.0.0.0:8080}" | awk -F: '{print $NF}' | tr -d '"')
 curl -fs "http://localhost:$PORT/health" || exit 1
